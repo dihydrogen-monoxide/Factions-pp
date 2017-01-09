004f01ee24a54514e25d49927e96d746d65fcd4d
HittmanA <hittmana@comcast.net>
2017-01-09 14:37:38 -0600

    Update MainClass.php

--- src/HittmanA/factionspp/MainClass.php
+++ src/HittmanA/factionspp/MainClass.php
@@ -6,22 +6,29 @@ use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
 
 class MainClass extends PluginBase implements Listener {
     public function onEnable() {
 	@mkdir($this->getDataFolder());
-  	$this->badPlayers = new Config($this->getDataFolder()."factions-list.txt", Config::ENUM);
+  	$facs = new Config($this->getDataFolder() . "factions.yml", Config::YAML);
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
 	$this->getServer()->getPluginManager()->registerEvents(new Events($this), $this);
         $this->getLogger()->info(TextFormat::YELLOW . "[FactionsPP] Loaded!");
     }
     public function onDisable() {
-        
+        $this->getLogger()->info(TextFormat::YELLOW . "[FactionsPP] Unloading!");
     }
     public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
         switch ($command->getName()) {
 		case "factionspp":
-			
+			$subcmd = $args[1];
+			if($subcmd == "create") {
+				$cfgfac = $args[2];
+				$write = array("name" => $cfgfac, "Leader" => $sender, "Officers" => array(), "Members" => array());
+				$facs->set($write);
+				$facs->save();
+			}
 			break;
 	    default:
                 return false;
