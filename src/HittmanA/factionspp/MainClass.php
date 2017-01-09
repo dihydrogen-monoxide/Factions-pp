a6e5653c2585837976b63251f78ab30c6640cc5e
HittmanA <hittmana@comcast.net>
2017-01-09 12:07:47 -0600

    Update MainClass.php

--- src/HittmanA/factionspp/MainClass.php
+++ src/HittmanA/factionspp/MainClass.php
@@ -1,1 +1,25 @@
 <?php
+namespace FactionsPP;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+
+class MainClass extends PluginBase implements Listener {
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+		    $this->getServer()->getPluginManager()->registerEvents(new Events($this), $this);
+        $this->getLogger()->info(TextFormat::YELLOW . "[FactionsPP] Loaded!");
+    }
+    public function onDisable() {
+        
+    }
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+        switch ($command->getName()) {
+            default:
+                return false;
+        }
+    }
+}
