d6be93a4828a6aa6f6200db6aa5c73b995f6006a
HittmanA <hittmana@comcast.net>
2017-01-11 18:46:43 -0600

    Update MainClass.php

--- src/HittmanA/factionspp/MainClass.php
+++ src/HittmanA/factionspp/MainClass.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace HittmanA\factionspp;
 
 use pocketmine\command\Command;
@@ -9,30 +10,33 @@ use pocketmine\utils\TextFormat;
 use pocketmine\utils\Config;
 
 class MainClass extends PluginBase implements Listener {
+
     public function onEnable() {
-	@mkdir($this->getDataFolder());
-	$this->saveResource("factions.json");
-  	$facs = new Config($this->getDataFolder() . "factions.json", Config::JSON);
+        $this->saveResource("factions.json");
+        $this->facs = new Config($this->getDataFolder() . "factions.json", Config::JSON);
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-	$this->getServer()->getPluginManager()->registerEvents(new Events($this), $this);
+        $this->getServer()->getPluginManager()->registerEvents(new Events($this), $this);
         $this->getLogger()->info(TextFormat::YELLOW . "[FactionsPP] Loaded!");
     }
+
     public function onDisable() {
         $this->getLogger()->info(TextFormat::YELLOW . "[FactionsPP] Unloading!");
     }
+
     public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
         switch ($command->getName()) {
-		case "factionspp":
-			$subcmd = $args[1];
-			if($subcmd == "create") {
-				$cfgfac = $args[2];
-				$write = array("name" => $cfgfac, "Leader" => $sender, "Officers" => array(), "Members" => array());
-				$facs->set($cfgfac,json_encode($write));
-				$facs->save();
-			}
-			break;
-	    default:
+        case "factionspp":
+            $subcmd = strtolower($args[1]);
+            if($subcmd == "create") {
+                $cfgfac = $args[2];
+                $write = array("name" => $cfgfac, "Leader" => $sender, "Officers" => array(), "Members" => array());
+                $this->facs->set($cfgfac, $write);
+                $this->$facs->save();
+            }
+            break;
+        default:
                 return false;
         }
     }
+
 }
