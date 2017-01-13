1c350b234592c6ac4409dd42508db80ca71a6616
HittmanA <hittmana@comcast.net>
2017-01-13 11:18:10 -0600

    Uggg I needed sleep

--- src/HittmanA/factionspp/MainClass.php
+++ src/HittmanA/factionspp/MainClass.php
@@ -112,9 +112,9 @@ class MainClass extends PluginBase implements Listener {
                                     $sender->sendMessage(TextFormat::GREEN . "You have left the faction!");
                                 }else{
                                     if($playerFPPProfile["role"]) {
-                                        $this->facs->$playerFac[$playerFPPProfile["rank"] . "s"]->remove($displayName);
-                                        $this->facs->$playerFac["faction"] = " ";
-                                        $this->facs->$playerFac["role"] = " ";
+                                        $this->facs->$playerFac[$playerFPPProfile["role"] . "s"]->remove($displayName);
+                                        $this->playerInfo->$displayName["faction"] = " ";
+                                        $this->playerInfo->$displayName["role"] = " ";
                                         $sender->sendMessage(TextFormat::GREEN . "You have left the faction!");
                                     }else{
                                         $sender->sendMessage(TextFormat::RED . "You must make another player leader first!");
