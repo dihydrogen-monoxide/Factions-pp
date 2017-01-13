886e0d7a5cf5fa82e985c305bb4bbf4ea36b70a0
HittmanA <hittmana@comcast.net>
2017-01-13 14:41:27 -0600

    Bug fixes
    Fixed a config saving issue. Also fixed a bug with /f info and checking
    if a player is in a fac. Next is cleaning up code.

--- src/HittmanA/factionspp/MainClass.php
+++ src/HittmanA/factionspp/MainClass.php
@@ -92,7 +92,7 @@ class MainClass extends PluginBase implements Listener {
                     }
 
                         elseif ($subcmd === "info") {
-                            if(isset($this->playerInfo->$displayName)) {
+                            if(isset($this->playerInfo->$displayName) && isset($this->playerInfo->$displayName["faction"])) {
                                 $playerFPPProfile = $this->playerInfo->$displayName;
                                 $playerFac = $playerFPPProfile["faction"];
                                 $playerFacInfo = $this->facs->$playerFac;
@@ -108,14 +108,12 @@ class MainClass extends PluginBase implements Listener {
                                 $playerFacInfo = $this->facs->$playerFac;
                                 if(empty($playerFacInfo["officers"]) || empty($playerFacInfo["members"])) {
                                     $this->facs->remove($playerFac);
-                                    $this->playerInfo->setNested($displayName, ["faction" => ""]);
-                                    $this->playerInfo->setNested($displayName, ["role" => ""]);
+                                    $this->playerInfo->setNested($displayName, ["name" => $displayName,"faction" => "","role" => ""]);
                                     $sender->sendMessage(TextFormat::GREEN . "You have left the faction!");
                                 }else{
-                                    if($playerFPPProfile["role"] === "Leader") {
+                                    if($playerFPPProfile["role"] !== "Leader") {
                                         $this->facs->$playerFac[$playerFPPProfile["role"] . "s"]->remove($displayName);
-                                        $this->playerInfo->setNested($displayName, ["faction" => ""]);
-                                        $this->playerInfo->setNested($displayName, ["role" => ""]); 
+                                        $this->playerInfo->setNested($displayName, ["name" => $displayName,"faction" => "","role" => ""]);
                                         $sender->sendMessage(TextFormat::GREEN . "You have left the faction!");
                                     }else{
                                         $sender->sendMessage(TextFormat::RED . "You must make another player leader first!");
@@ -125,8 +123,6 @@ class MainClass extends PluginBase implements Listener {
                                 $sender->sendMessage(TextFormat::RED . "You must be part of a faction to run this command!");
                             }
                         }
-                        $this->facs->save(true);
-                        $this->playerInfo->save(true);
                 } else {
                     $sender->sendMessage(TextFormat::RED . "Please run this command in-game");
                 }
