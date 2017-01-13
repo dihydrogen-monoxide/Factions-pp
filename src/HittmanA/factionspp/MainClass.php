e134cc6532af356520a8d47d6270c865dd403893
JunkDaCoder <junkdudeofficial@gmail.com>
2017-01-12 19:55:11 -0700

    Update MainClass.php
    Check if the faction exists

--- src/HittmanA/factionspp/MainClass.php
+++ src/HittmanA/factionspp/MainClass.php
@@ -13,89 +13,96 @@ use pocketmine\utils\Config;
 
 class MainClass extends PluginBase implements Listener {
 
-	/** @var Config */
-	protected $fac;
-	//Faction name tag coming soon >:D
-	//$player->setDisplayName("My Rank" . $player->getName());
-	public function onEnable() {
-		@mkdir($this->getDataFolder());
-		$this->facs = new Config($this->getDataFolder() . "factions.json", Config::JSON, []);
-		$this->playerInfo = new Config($this->getDataFolder() . "players.json", Config::JSON, []);
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getServer()->getPluginManager()->registerEvents(new Events($this), $this);
-		$this->getLogger()->info(TextFormat::YELLOW . "Loaded!");
-	}
+    /** @var Config */
+    protected $fac;
+    //Faction name tag coming soon >:D
+    //$player->setDisplayName("My Rank" . $player->getName());
+    public function onEnable() {
+        @mkdir($this->getDataFolder());
+        $this->facs = new Config($this->getDataFolder() . "factions.json", Config::JSON, []);
+        $this->playerInfo = new Config($this->getDataFolder() . "players.json", Config::JSON, []);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getPluginManager()->registerEvents(new Events($this), $this);
+        $this->getLogger()->info(TextFormat::YELLOW . "Loaded!");
+    }
 
-	public function onDisable() {
-		$this->getLogger()->info(TextFormat::YELLOW . "Unloading!");
-	}
+    public function onDisable() {
+        $this->getLogger()->info(TextFormat::YELLOW . "Unloading!");
+    }
 
-	public function onJoin(PlayerJoinEvent $event) {
-		$player = $event->getPlayer();
-		$dispName = $player->getName();
-		$fac = $this->playerInfo->$dispName->faction;
-		if(isset($fac)) {
-			$prefix = "[$fac]";
-			$player->setDisplayName($prefix . " " . $player->getName());
-			$player->setNameTag($prefix . " " . $player->getName());
-		}
-	}
+    public function onJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $dispName = $player->getName();
+        $fac = $this->playerInfo->$dispName->faction;
+        if(isset($fac)) {
+            $prefix = "[$fac]";
+            $player->setDisplayName($prefix . " " . $player->getName());
+            $player->setNameTag($prefix . " " . $player->getName());
+        }
+    }
+    ##$this->facs->$facWhosPowerYouWantToChange["power"] == 10;
+##$this->facs->save(true);
 
-	public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
-		$displayName = $sender->getName();
-		$subcmd = strtolower(array_shift($args));
-		switch ($command->getName()) {
-			case "factionspp":
-			case "fpp":
-			case "f":
-				if($sender instanceof Player) {
-						if($subcmd === "create") {
-							if(isset($args[0])) {
-								if(isset($this->playerInfo->$displayName["faction"])) {
-									$sender->sendMessage(TextFormat::RED . "You are already in a faction!");
-								}else{
-									$facName = array_shift($args);
-									$this->facs->set($facName, [
-										"name" => strtolower($facName),
-										"display" => $facName,
-										"leader" => $displayName,
-										"officers" => [],
-										"members" => [],
-										"power" => 5
-									]);
-									$this->playerInfo->set($displayName,[
-										"name" => $displayName,
-										"faction" => $facName,
-										"role" => "Leader"
-									]);
-									$this->facs->save(true);
-									$this->playerInfo->save(true);
-									$prefix = "[".$facName."]";
-									$sender->setDisplayName($prefix . " " . $displayName);
-									$sender->setNameTag($prefix . " " . $displayName);
-									$sender->sendMessage(TextFormat::GREEN . "Faction created!");
-								}
-							} else {
-								$sender->sendMessage(TextFormat::GOLD . "Usage: /factionspp create <name>");
-							}
-						}elseif ($subcmd === "info") {
-							if(isset($this->playerInfo->$displayName)) {
-								$playerFPPProfile = $this->playerInfo->$displayName;
-								$playerFac = $playerFPPProfile["faction"];
-								$playerFacInfo = $this->facs->$playerFac;
-								$sender->sendMessage(TextFormat::GOLD . "Faction: " . $playerFac);
-								$sender->sendMessage(TextFormat::GREEN . "Your Role: " . $playerFPPProfile["role"]);
-							}else{
-								$sender->sendMessage(TextFormat::RED . "You must be part of a faction to run this command!");
-							}
-						}
-				} else {
-					$sender->sendMessage(TextFormat::RED . "Please run this command in-game");
-				}
-				return true;
-			default:
-				return false;
-		}
-	}
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+        $displayName = $sender->getName();
+        $subcmd = strtolower(array_shift($args));
+        switch ($command->getName()) {
+            case "factionspp":
+            case "fpp":
+            case "f":
+                if($sender instanceof Player) {
+                        if($subcmd === "create") {
+                            if(isset($args[0])) {
+                                $facName = array_shift($args);
+                                if(isset($this->playerInfo->$displayName["faction"])) {
+                                    $sender->sendMessage(TextFormat::RED . "You are already in a faction!");
+                                }else
+                                if(isset($this->facs->$facName)){
+                                    $sender->sendMessage(TextFormat::RED . "A faction with this name already exists!");
+                                }elseif(!isset($this->playerInfo->$displayName["faction"])) {
+                                    if(!isset($this->facs->$facName)){
+                                    $this->facs->set($facName, [
+                                        "name" => strtolower($facName),
+                                        "display" => $facName,
+                                        "leader" => $displayName,
+                                        "officers" => [],
+                                        "members" => [],
+                                        "power" => 5
+                                    ]);
+                                    $this->playerInfo->set($displayName,[
+                                        "name" => $displayName,
+                                        "faction" => $facName,
+                                        "role" => "Leader"
+                                    ]);
+                                    $this->facs->save(true);
+                                    $this->playerInfo->save(true);
+                                    $prefix = "[".$facName."]";
+                                    $sender->setDisplayName($prefix . " " . $displayName);
+                                    $sender->setNameTag($prefix . " " . $displayName);
+                                    $sender->sendMessage(TextFormat::GREEN . "Faction created!");
+                                }
+                            } else {
+                                $sender->sendMessage(TextFormat::GOLD . "Usage: /factionspp create <name>");
+                            }
+                        }
+                        }elseif ($subcmd === "info") {
+                            if(isset($this->playerInfo->$displayName)) {
+                                $playerFPPProfile = $this->playerInfo->$displayName;
+                                $playerFac = $playerFPPProfile["faction"];
+                                $playerFacInfo = $this->facs->$playerFac;
+                                $sender->sendMessage(TextFormat::GOLD . "Faction: " . $playerFac);
+                                $sender->sendMessage(TextFormat::GREEN . "Your Role: " . $playerFPPProfile["role"]);
+                            }else{
+                                $sender->sendMessage(TextFormat::RED . "You must be part of a faction to run this command!");
+                            }
+                        }
+                } else {
+                    $sender->sendMessage(TextFormat::RED . "Please run this command in-game");
+                }
+                return true;
+            default:
+                return false;
+        }
+    }
 
 }
