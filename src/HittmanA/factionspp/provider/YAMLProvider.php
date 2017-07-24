4588e9f26eac625d25d92cc3e03fd5e06fd7a408
jasonwynn10 <jasonwynn10@me.com>
2017-07-23 22:04:27 -0400

    added default return value to prevent errors

--- src/HittmanA/factionspp/provider/YAMLProvider.php
+++ src/HittmanA/factionspp/provider/YAMLProvider.php
@@ -34,12 +34,7 @@ class YAMLProvider extends BaseProvider implements Provider{
     public function getFaction(string $factionName): array
     {
         $name = strtolower($factionName);
-        if($this->factions->get($name) == false)
-        {
-            return array();
-        } else {
-            return $this->factions->get($name);
-        }
+        return $this->factions->get($name, []);
     }
     
     public function getPlayer(IPlayer $player): array
